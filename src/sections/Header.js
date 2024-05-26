6f131de1e46f5151a6088e92679d77623740f21c
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-05-26 19:48:44 -0300

    added light and dark mode

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,9 +1,38 @@
 import './header.css'
+import lightLogo from '../components/dark-logo.png'
+import darkLogo from '../components/light-logo.png'
+import useLocalStorage from 'use-local-storage'
 
 export default function Header() {
+    const preference = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const [isLight, setIsLight] = useLocalStorage('isLight', preference)
+
+
     return (
         <div className='header'>
-            <h3>header</h3>
+            <img
+                src={isLight ? lightLogo : darkLogo}
+                alt='paper-airplane-png'>
+            </img>
+
+            <Toggle
+                isChecked={isLight}
+                handleChange={() => setIsLight(!isLight)}
+            />
         </div>
     )
+}
+
+const Toggle = ({ handleChange, isChecked }) => {
+    return (
+        <div className='toggle-container'>
+            <input
+                type='checkbox'
+                id='check'
+                className='toggle'
+                onChange={handleChange}
+                checked={isChecked} />
+            <label htmlFor='check'>{isChecked ? 'Light Mode' : 'Dark Mode'}</label>
+        </div>
+    );
 }
--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -1,17 +1,21 @@
 import './hero-section.css'
 import EarthAnimation from '../components/Animation Earth.json'
+import EarthAnimationDark from '../components/Animation EarthDark.json'
 import PlaneAnimation from '../components/Animation plane.json'
 import Lottie from 'lottie-react'
 import { useRef } from 'react';
+import useLocalStorage from 'use-local-storage'
 
 export default function HeroSection() {
+    const preference = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const [isLight, setIsLight] = useLocalStorage('isLight', preference)
 
     return (
         <div className='section'>
             <div className='container'>
                 <Lottie
                     className='earth-animation'
-                    animationData={EarthAnimation}
+                    animationData={isLight ? EarthAnimationDark : EarthAnimation}
                 />
                 {/* <Lottie
                     className='earth-animation'
@@ -19,7 +23,7 @@ export default function HeroSection() {
                 <div className='hero-text'>
                     <h1>Earth Roulette</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita non alias ut saepe iusto hic qui consequatur nostrum nemo similique.</p>
-                    <button>Button</button>
+                    <button>Spin</button>
                 </div>
             </div>
         </div>
