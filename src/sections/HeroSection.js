ee09188efb755bcdb8ee26334dc797b0de2809c6
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-06-21 12:24:59 -0300

    updated hero section text

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -20,8 +20,9 @@ export default function HeroSection() {
                     animationData={PlaneAnimation} /> */}
                 <div className='hero-text'>
                     <h1>Earth Roulette</h1>
-                    <p>Hey there! Feeling the wanderlust but not sure where to go next? Let me help! This app will whisk you away on a virtual adventure by selecting a random country for you to explore. <br />   But that's not all! I'll also show you the current weather there to help you pack, and highlight some upcoming dates and events to get your trip planning started. Think the chosen destination sounds like your dream vacation?  No problem! I'll even provide a quick link to booking flights, so you can turn this virtual trip into reality.
-                        Ready to discover your next adventure? Let's get started!</p>
+                    <p>Hey there! Feeling the wanderlust but not sure where to go next? Let me help! This app will whisk you away on a virtual adventure by selecting a random country for you to explore.</p>
+                    <p>But that's not all! I'll also show you the current weather there to help you pack, and highlight some upcoming dates and events to get your trip planning started.</p>
+                    <p>Plus, with the power of AI, I can provide you with a personalized summary of the chosen country, including fascinating facts, must-do activities, and hidden gems. Think the chosen destination sounds like your dream vacation?  No problem! I'll even provide a quick link to booking flights, so you can turn this virtual trip into reality.</p>
                     <Button />
                 </div>
             </div>
