cb24f49224047f416246c859a3a505ba5e77bd76
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-06-28 12:06:03 -0300

    travelbot test

--- src/sections/TravelBot.js
+++ src/sections/TravelBot.js
@@ -1,7 +1,8 @@
 import '../sections/styles/travelbot.css'
 import { useEffect, useState } from 'react';
 
-export default function TravelBot({ info, loading }) {
+export default function TravelBot({ info }) {
+    const [loading, setLoading] = useState()
     const loadingClass = loading ? 'loading' : '';
     const [value, setValue] = useState('');
     const [conversationHistory, setConversationHistory] = useState([]);
@@ -21,12 +22,14 @@ export default function TravelBot({ info, loading }) {
         }
 
         try {
+            setLoading(true)
             const response = await fetch('https://earthroulette.net/.netlify/functions/botreply', options);
             // const response = await fetch('http://localhost:8000/reply', options)
             const data = await response.json();
             const content = data?.choices[0]?.message?.content
             setAiReply(content);
-            setConversationHistory([...conversationHistory, { userQuestion: value, aiReply: content }]);
+            setConversationHistory([{ userQuestion: value, aiReply: content }, ...conversationHistory,]);
+            setLoading(false)
 
         } catch (error) {
             console.error('Error making API call:', error);
@@ -35,8 +38,10 @@ export default function TravelBot({ info, loading }) {
     };
 
     useEffect(() => {
-
-    }, [aiReply])
+        if (country) {
+            setConversationHistory([]); // Clear conversation history on new country
+        }
+    }, [country]);
 
     console.log(error)
 
